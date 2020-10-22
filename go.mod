b0c5c4d06e55bc1cfea22797c3d9bb2f0774da29
Jan Hellar <jhellar@redhat.com>
2020-10-22 11:18:54 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,30 @@
+module github.com/jhellar/remote-write
+
+go 1.15
+
+require (
+	github.com/Azure/go-autorest/autorest v0.11.10 // indirect
+	github.com/aws/aws-sdk-go v1.35.10 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/gogo/protobuf v1.3.1
+	github.com/golang/snappy v0.0.2
+	github.com/gophercloud/gophercloud v0.13.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.15.2 // indirect
+	github.com/hashicorp/consul/api v1.7.0 // indirect
+	github.com/miekg/dns v1.1.34 // indirect
+	github.com/prometheus/client_golang v1.8.0 // indirect
+	github.com/prometheus/common v0.14.0
+	github.com/prometheus/prometheus v1.8.2-0.20201015110737-0a7fdd3b7696
+	github.com/samuel/go-zookeeper v0.0.0-20200724154423-2164a8ac840e // indirect
+	golang.org/x/net v0.0.0-20201020065357-d65d470038a5 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	google.golang.org/api v0.33.0 // indirect
+	google.golang.org/genproto v0.0.0-20201019141844-1ed22bb0c154 // indirect
+	google.golang.org/grpc v1.33.0 // indirect
+	google.golang.org/protobuf v1.25.0
+	gopkg.in/fsnotify/fsnotify.v1 v1.4.7 // indirect
+	k8s.io/api v0.19.3 // indirect
+	k8s.io/client-go v11.0.0+incompatible // indirect
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20201015054608-420da100c033 // indirect
+)
---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/pkg/timestamp"
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/prometheus/prometheus/storage/remote"
+)
+
+type ombResults struct {
+	ConsumeRate         []float64 `json:"consumeRate"`
+	EndToEndLatencyAvg  []float64 `json:"endToEndLatencyAvg"`
+	PublishLatency99pct []float64 `json:"publishLatency99pct"`
+	PublishRate         []float64 `json:"publishRate"`
+}
+
+type sample struct {
+	labels labels.Labels
+	t      int64
+	v      float64
+}
+
+func labelsToLabelsProto(labels labels.Labels, buf []prompb.Label) []prompb.Label {
+	result := buf[:0]
+	if cap(buf) < len(labels) {
+		result = make([]prompb.Label, 0, len(labels))
+	}
+	for _, l := range labels {
+		result = append(result, prompb.Label{
+			Name:  l.Name,
+			Value: l.Value,
+		})
+	}
+	return result
+}
+
+func addSamples(data []float64, metricName string, timeseries *prompb.TimeSeries, additionalLabels labels.Labels) {
+	for i := range data {
+		sample := sample{
+			labels: append(labels.Labels{{Name: "__name__", Value: metricName}}, additionalLabels...),
+			t:      timestamp.FromTime(time.Now().Add(time.Second * time.Duration(-10*(len(data)-i)))),
+			v:      data[i],
+		}
+
+		timeseries.Labels = labelsToLabelsProto(sample.labels, timeseries.Labels)
+		timeseries.Samples[i].Timestamp = sample.t
+		timeseries.Samples[i].Value = sample.v
+	}
+}
+
+func main() {
+	thanosPtr := flag.String("thanos", "", "Thanos URL.")
+	jsonPtr := flag.String("results", "", "OMB results json path.")
+	labelsPtr := flag.String("labels", "", "Additional label:value pairs (separated by comma).")
+
+	flag.Parse()
+
+	thanosURL := *thanosPtr
+	if thanosURL == "" {
+		thanosURL = os.Getenv("THANOS_RECEIVER_URL")
+	}
+
+	labelsArr := strings.Split(*labelsPtr, ",")
+
+	additionalLabels := make(labels.Labels, len(labelsArr))
+
+	for i := range labelsArr {
+		pair := strings.Split(labelsArr[i], ":")
+		additionalLabels[i] = labels.Label{Name: pair[0], Value: pair[1]}
+	}
+
+	file, _ := ioutil.ReadFile(*jsonPtr)
+
+	results := ombResults{}
+
+	err := json.Unmarshal([]byte(file), &results)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	serverURL, err := url.Parse(thanosURL + "/api/v1/receive")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conf := &remote.ClientConfig{
+		URL:     &config.URL{URL: serverURL},
+		Timeout: model.Duration(time.Second),
+	}
+
+	c, err := remote.NewWriteClient("load-test", conf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	timeseries := make([]prompb.TimeSeries, 4)
+	for i := range timeseries {
+		timeseries[i].Samples = make([]prompb.Sample, len(results.ConsumeRate))
+	}
+
+	addSamples(results.ConsumeRate, "omb_results_consume_rate", &timeseries[0], additionalLabels)
+	addSamples(results.EndToEndLatencyAvg, "omb_results_end_to_end_latency_avg", &timeseries[1], additionalLabels)
+	addSamples(results.PublishLatency99pct, "omb_results_publish_latency_99pct", &timeseries[2], additionalLabels)
+	addSamples(results.PublishRate, "omb_results_publish_rate", &timeseries[3], additionalLabels)
+
+	var buf []byte
+
+	req := &prompb.WriteRequest{
+		Timeseries: timeseries,
+	}
+
+	data, err := proto.Marshal(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	compressed := snappy.Encode(buf, data)
+
+	buf = compressed
+
+	err = c.Store(context.Background(), buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
